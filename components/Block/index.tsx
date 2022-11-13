41422e0dd6ac3f894d3cd88118a7191235a1997a
Luis Espinoza <luis@stakeordie.com>
2022-11-12 19:40:49 -0800

    Cleaning up components

--- components/Block/index.tsx
+++ components/Block/index.tsx
@@ -4,11 +4,11 @@ type Props = {
   children: React.ReactNode;
 };
 
-export const Block: React.FC<Props> = ({ children }: Props) => {
+export const Block: React.FC<Props> = ({ children }) => {
   return <div className="py-2">{children}</div>;
 };
 
-export const HCenteredBlock: React.FC<Props> = ({ children }: Props) => {
+export const HCenteredBlock: React.FC<Props> = ({ children }) => {
   return <div className="flex items-center h-full">{children}</div>;
 };
 
--- components/GameScreen/index.tsx
+++ components/GameScreen/index.tsx
@@ -10,6 +10,7 @@ const GameScreen: React.FC<Props> = () => {
   function onCommand(value: string) {
     setData([...data, value]);
   }
+
   return (
     <div className="flex flex-col justify-between border-2 py-2">
       <div className="px-2 pb-2 h-[400px]">
